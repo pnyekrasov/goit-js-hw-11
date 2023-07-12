8663a161307168bbb5e060e0fd4885eceaaa0e43
pnyekrasov <pnekrasov1910@gmail.com>
2023-07-12 20:09:46 +0300

    start 2

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,6 @@
-API_KAY = 38184574-73f03994b4792e0e0e3ddcdab;
+// API_KAY = 38184574-73f03994b4792e0e0e3ddcdab;
+const formEl = document.querySelector('search-form');
+
+formEl.addEventListener('submit', halderSearchImages);
+
+halderSearchImages;
