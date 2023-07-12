cfefdbf653b5edf7ff6458f28629ecf5378f23c8
pnyekrasov <126099923+pnyekrasov@users.noreply.github.com>
2023-07-12 23:19:48 +0000

    Deploying to gh-pages from @ pnyekrasov/goit-js-hw-11@9a68acfed10dae5b8dec601e9f825e5a846f7684 🚀

--- index.137f3e2c.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("search-form").addEventListener("submit",halderSearchImages),halderSearchImages;
-//# sourceMappingURL=index.137f3e2c.js.map
--- index.196448c3.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("search-form").addEventListener("submit",halderSearchImages),halderSearchImages;
-//# sourceMappingURL=index.196448c3.js.map
