9a68acfed10dae5b8dec601e9f825e5a846f7684
pnyekrasov <pnekrasov1910@gmail.com>
2023-07-13 02:19:18 +0300

    HW 11

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,130 @@
-// API_KAY = 38184574-73f03994b4792e0e0e3ddcdab;
-const formEl = document.querySelector('search-form');
+import { PixabayAPI } from './pixabay-api';
 
-formEl.addEventListener('submit', halderSearchImages);
+const searchFormEl = document.querySelector('.search-form');
+const galleryListEl = document.querySelector('.gallery');
+const loadMoreBtnEl = document.querySelector('.load-more');
 
-halderSearchImages;
+loadMoreBtnEl.hidden = true;
+
+const pixabayReturn = new PixabayAPI();
+
+searchFormEl.addEventListener('submit', handlerSearchImages);
+
+function handlerSearchImages(e) {
+  e.preventDefault();
+  const {
+    elements: { searchQuery },
+  } = e.currentTarget;
+  const textInput = searchQuery.value.trim();
+  if (!textInput) {
+    console.log('!');
+    return;
+  }
+
+  pixabayReturn.query = textInput;
+  pixabayReturn
+    .searchImages()
+    .then(data => {
+      if (data.total === 0) {
+        console.log(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+        return;
+      }
+
+      console.log(data.total);
+
+      function createGallaryCards(array) {
+        return array
+          .map(
+            ({
+              webformatURL,
+              largeImageURL,
+              tags,
+              likes,
+              views,
+              comments,
+              downloads,
+            }) => `<li class="photo-card">
+        <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+        <div class="info">
+          <p class="info-item">
+            <b>Likes</b>${likes}
+          </p>
+          <p class="info-item">
+            <b>Views</b>${views}
+          </p>
+          <p class="info-item">
+            <b>Comments</b>${comments}
+          </p>
+          <p class="info-item">
+            <b>Downloads</b>${downloads}
+          </p>
+        </div>
+      </li>`
+          )
+          .join('');
+      }
+
+      galleryListEl.innerHTML = createGallaryCards(data.hits);
+
+      loadMoreBtnEl.hidden = false;
+    })
+    .catch(console.warm);
+}
+
+loadMoreBtnEl.addEventListener('click', handlerLoadMoreImages);
+
+function handlerLoadMoreImages() {
+  pixabayReturn.page += 1;
+  pixabayReturn
+    .searchImages()
+    .then(data => {
+      if (
+        pixabayReturn.page === Math.ceil(data.total / pixabayReturn.per_page)
+      ) {
+        console.log(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+        loadMoreBtnEl.hidden = true;
+      }
+
+      function createGallaryCards(array) {
+        return array
+          .map(
+            ({
+              webformatURL,
+              largeImageURL,
+              tags,
+              likes,
+              views,
+              comments,
+              downloads,
+            }) => `<li class="photo-card">
+        <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+        <div class="info">
+          <p class="info-item">
+            <b>Likes</b>${likes}
+          </p>
+          <p class="info-item">
+            <b>Views</b>${views}
+          </p>
+          <p class="info-item">
+            <b>Comments</b>${comments}
+          </p>
+          <p class="info-item">
+            <b>Downloads</b>${downloads}
+          </p>
+        </div>
+      </li>`
+          )
+          .join('');
+      }
+
+      galleryListEl.insertAdjacentHTML(
+        'beforeend',
+        createGallaryCards(data.hits)
+      );
+    })
+    .catch(console.warm);
+}
---
+++ src/js/pixabay-api.js
@@ -0,0 +1,24 @@
+'use strict';
+
+export class PixabayAPI {
+  #API_KAY = '38184574-73f03994b4792e0e0e3ddcdab';
+  #BASE_URL = 'https://pixabay.com/api/';
+  #CONST_PARAMETERS = 'image_type=photo&orientation=horizontal&safesearch=true';
+
+  query = null;
+  page = 1;
+  per_page = 40;
+
+  searchImages() {
+    return fetch(
+      `${this.#BASE_URL}?key=${this.#API_KAY}&q=${this.query}&${
+        this.#CONST_PARAMETERS
+      }&page=${this.page}&per_page=${this.per_page}`
+    ).then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    });
+  }
+}
